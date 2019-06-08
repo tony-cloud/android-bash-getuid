6e89149d6232ce7b50ddf722e5a4a289d0bd53a4
Tony <lj123copyright3@gmail.com>
2019-06-09 03:46:55 +0800

    refactor and improve
    1. Use "cut" command to cut the line
    2 Try to remove unexpected /r from windows notepad
    3. Add warning

--- finduid.sh
+++ finduid.sh
@@ -1,4 +1,13 @@
 #!/system/bin/sh
-apkname=$1
 #find uid in packages.list, need root
-cat /data/system/packages.list |grep $apkname |sed "s/$apkname //g" |sed 's/ .*//g'
+app_uid=`grep "$1 " /data/system/packages.list | cut -d ' ' -f 2`
+app_uid=`echo $app_uid | sed 's/\r//g'`
+if [ "${app_uid}" != "" ] ; then
+  if [ -n "`echo $app_uid | sed -n '/^[0-9][0-9]*$/p'`" ] ; then
+    echo $app_uid
+    return
+  fi
+else
+  echo "[Warning]: package $1 not found."
+  return -1
+fi
